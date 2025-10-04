da965a4635b38680e3c4de40c5aec44dd363ba82
Zaid Asseh <zeid.2007ilovxbox@gmail.com>
2025-10-04 23:18:27 +0300

    Added the new communicator interface

--- internal/smux/constants.go
+++ internal/smux/constants.go
@@ -1,9 +1,30 @@
 package smux
 
-import "sync"
+import (
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+type Communicator interface {
+	net.Conn
+
+	Send(b []byte, timeout time.Duration) error
+	NewStreamedSender(timeout time.Duration) io.WriteCloser
+	SendSerialized(b []byte, timeout time.Duration) error
+	SendEncrypted(b []byte, timeout time.Duration) error
+	NewStreamedEncryptedSender(timeout time.Duration) (io.WriteCloser, error)
+
+	Receive(b []byte, timeout time.Duration) error
+	NewStreamedReceiver(timeout time.Duration) io.ReadCloser
+	ReceiveSerialized(b []byte, timeout time.Duration) (uint64, error)
+	ReceiveEncrypted(b []byte, timeout time.Duration) (uint64, error)
+	NewStreamedEncryptedReceiver(timeout time.Duration) (io.ReadCloser, error)
+}
 
 var headerPool = sync.Pool{
-	New: func() interface{} {
+	New: func() any {
 		return make([]byte, 2)
 	},
 }
